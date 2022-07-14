48b7ee6f3309851468d07c4f26bf620902c7638d
farhan <farhnn.r@gmail.com>
2022-07-14 07:23:41 +0700

    endpoin delete product

--- app/Controllers/Products.php
+++ app/Controllers/Products.php
@@ -34,7 +34,7 @@ class Products extends ResourceController
             $nameImg = $fileImg->getRandomName();
             $fileImg->move('img', $nameImg);
         }
-        
+
         $this->productsModel->save([
             'product_name' => $this->request->getVar('product_name'),
             'product_stock' => $this->request->getVar('product_stock'),
@@ -53,8 +53,29 @@ class Products extends ResourceController
 
         return $this->respondCreated($response, 'Success');
     }
+
     public function delete($id = null)
     {
-        //
+        $dataProduct = $this->productsModel->find($id);
+
+        if(!$dataProduct) {
+            return $this->fail('Product tidak ditemukan');
+        }
+
+        if($dataProduct['product_img'] != 'default.jpg'){
+            unlink('img/' . $dataProduct['product_img']);
+        }
+
+        $this->productsModel->delete($id);
+
+        $response = [
+            'status'   => 201,
+            'error'    => null,
+            'messages' => [
+                'success' => 'Product berhasil dihapus'
+            ]
+        ];
+
+        return $this->respondDeleted($response);
     }
 }
